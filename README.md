2f519573fa91342a8f10ce19ea7ad8cd19dbdc8d
John Spawn <jolivet.anthony23666@gmail.com>
2023-03-22 16:08:18 +0100

    Ajout nouvelles fonctionnalités java14

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # java14
 New of java #14
+
+- Class record : Equivaut a une classe avec champs private final, constructeur public, getters, setters, equals(), hashCodes(), toString().
+Ils ne peuvent pas hériter d'autres classes, ni contenir des champs supplémentaires que ceux décrivant l'état du record.
+
+- Switch amélioré
+
+- Text Blocks
+
+-> Migration sans problème sauf si utilisation de Pack200 ou ConcurrentMarkSweep.
+Ce n'est par contre pas une version LTS.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
+
+
+
+    record Range(int lo, int hi) {
+
+        public Range {
+            if (lo < hi){
+                throw new IllegalArgumentException(String.format("(%d,%d)", lo, hi));
+            }
+        }
+
+    }
+
+    static void updateSwitch(String day){
+        switch (dat){
+            case "MONDAY", "FRIDAY", "SUNDAY" -> System.out.println(6);
+            case "TUESDAY" -> System.out.println(7);
+            case "THURSDAY", "SATURDAY" -> System.out.println(8);
+            case "WEDNESDAY" -> System.out.println(9);
+        }
+    }
+
+    static void updateSwitch2(String day){
+
+        int j = switch (day){
+            case "MONDAY" -> 0;
+            case "TUESDAY" -> 1;
+            default ->  {
+                int k = day.length();
+                yield k;
+            }
+        };
+    }
+
+    void textBlocks(){
+
+        String text = """
+                fdsmfgfd fgdgfdg gfdgfdgfd gfdgfd
+                gfdgfd gfdgfd gfdgfdg gfdgfd gfd
+                """;
+
+        String textWithEspace = """
+               red /s
+               green /s
+               blue /s
+                """ ;
+
+    }
+
 }
